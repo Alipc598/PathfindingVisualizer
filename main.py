604d20d6d3c80c5fb56f80d41ff3437a9b4ec450
Alipc598 <117017807+Alipc598@users.noreply.github.com>
2024-01-21 09:11:11 +0100

    Structured

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+src_dir = os.path.join(os.path.dirname(__file__), 'src')
+sys.path.insert(0, src_dir)
+
 from app import PathfindingVisualizerApp
 
 if __name__ == '__main__':
